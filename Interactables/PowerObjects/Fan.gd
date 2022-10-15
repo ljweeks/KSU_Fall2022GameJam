56eed88ca34c81aba8d51d84fe340df3e51be190
lexweeks <lexweeks@gmail.com>
2022-10-15 10:21:10 -0500

    added fan heat

--- Interactables/PowerObjects/Fan.gd
+++ Interactables/PowerObjects/Fan.gd
@@ -1,7 +1,7 @@
 extends StaticBody2D
 
 onready var powerableObject = $PowerableObject as PowerableObject
-
+onready var Game = get_node("/root/Game")
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -10,10 +10,15 @@ onready var powerableObject = $PowerableObject as PowerableObject
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+export (float) var tickRate = 1
+var tick = 0
 
-
+export (float) var coolPower = 5
 func _process(delta):
+	tick += delta
 	if powerableObject.powered:
-		print("powered")
+		if tick > tickRate:
+			Game.overload -= coolPower
+			tick = 0
 	else:
 		pass
