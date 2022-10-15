dce5d0395ed8856348ec5e1b8b7f01049cc4ad7d
Cdaggson <cdaggson@ksu.edu>
2022-10-15 10:06:57 -0500

    plug progress

---
+++ Interactables/PowerObjects/Outlet.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+var current_plug = null
+var collision_disable_timer = 0
+
+func _process(delta):
+	if collision_disable_timer > 0:
+		collision_disable_timer -= delta
+		if collision_disable_timer <= 0:
+			$CollisionShape2D.disabled = false
+
+func release_plug():
+	if (current_plug != null):
+		$CollisionShape2D.disabled = true
+		current_plug.plugged = false
+		collision_disable_timer = 1
+
+func _on_Area2D_body_entered(body):
+	if body is PlugEnd and current_plug == null:
+		body.global_position = global_position
+		body.plugged = true
+		current_plug = body
---
+++ Interactables/PowerObjects/PlugEnd.gd
@@ -0,0 +1,10 @@
+extends RigidBody2D
+class_name PlugEnd
+
+var plugged = false setget on_plug
+
+func on_plug(new_value):
+	if (new_value):
+		set_mode(RigidBody2D.MODE_STATIC)
+	else:
+		set_mode(RigidBody2D.MODE_RIGID)
